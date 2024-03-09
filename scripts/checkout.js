80f45cf290ac34d4007ef9a44fe70eeef56990e7
jurabek10 <samiev.jurabek@bk.ru>
2024-03-09 22:52:03 +0900

    Delete product from HTML when clicking

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -16,7 +16,8 @@ cart.forEach((cartItem) => {
   });
 
   cartSummaryHTML += `
-    <div class="cart-item-container">
+    <div class="cart-item-container 
+    js-cart-item-container-${matchingProduct.id}">
         <div class="delivery-date">Delivery date: Wednesday, June 15</div>
 
         <div class="cart-item-details-grid">
@@ -98,5 +99,10 @@ document.querySelectorAll(".js-delete-link").forEach((link) => {
   link.addEventListener("click", () => {
     const productId = link.dataset.productId;
     removeFromCart(productId);
+
+    const container = document.querySelector(
+      `.js-cart-item-container-${productId}`
+    );
+    container.remove();
   });
 });
